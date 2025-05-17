336cb7bfb2c79536d70253715ccc67b4a43c3411
DonizeteVida <donizetevida1998@outlook.com>
2025-05-17 17:55:46 +0200

    Add AOSP FRROManager project

--- Drink/rro/build.gradle.kts
+++ Drink/rro/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.vendor.drink.rro"
-        minSdk = 21
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
