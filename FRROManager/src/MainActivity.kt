87b980bd04f86cf2c7509fb2c8dac8a82486d648
DonizeteVida <donizetevida1998@outlook.com>
2025-08-19 17:54:55 +0200

    Add FabricatedOverlay example

--- FRROManager/src/MainActivity.kt
+++ FRROManager/src/MainActivity.kt
@@ -1,7 +1,46 @@
 package com.vendor.frro.manager
 
+import android.content.om.FabricatedOverlay
+import android.content.om.OverlayManager
+import android.content.om.OverlayManagerTransaction
+import android.os.Bundle
+import android.util.TypedValue
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button)?.setOnClickListener {
+            val packageService = getSystemService<OverlayManager>()!!
+
+            val fabricatedOverlay = FabricatedOverlay.Builder(
+                packageName,
+                "ThemeResources",
+                "com.vendor.drink.default"
+            )
+                .setTargetOverlayable("ThemeResources")
+                .build()
+                .apply {
+                    setResourceValue(
+                        "string/hello_world",
+                        TypedValue.TYPE_STRING,
+                        "Hello World from FRRO",
+                        null
+                    )
+                }
+
+            val transaction = OverlayManagerTransaction
+                .Builder()
+                .registerFabricatedOverlay(fabricatedOverlay)
+                .setEnabled(fabricatedOverlay.identifier, true)
+                .build()
+
+            packageService.commit(transaction)
+        }
+    }
 }
